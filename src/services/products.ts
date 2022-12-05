57271dd2ab45b27e9955c53a6f6d784a42c8d653
Dima <ddikey1@gmail.com>
2022-12-05 12:47:45 +0200

    test commit to check reason of netlify error v 1.3

--- src/services/products.ts
+++ src/services/products.ts
@@ -17,7 +17,7 @@ export const getProductsByQuery = async (
     perPage: ProductsPerPage = ProductsPerPage.TwentyFour
 ) => {
   let products = await getAllProducts(ALL_PHONES_PATH);
-  const productsQuantity = products?.length;
+  const productsQuantity = products && products.length;
 
   if (!products) {
     return null;
